1edd786d241608df6c94bdb2c4a89c47d461c6a1
Sebastian Niehus <Sebastian.Niehus@bihealth.de>
2020-02-26 09:02:13 +0100

    Add parser options for sequencing platform.
    Add parser option for sequencing platform.
    Add parer option for structure-string.
    Remove parser ooption for barcode length, as it is implicitly
    included in platform or stucture string.
    Add struct ReadStructure that hold the information on
    the structure of a read pair and the position of barcode, sequence etc.
    Started implementation of parsing function for the structure string (work in progress).

--- src/command_line_parsing.h
+++ src/command_line_parsing.h
@@ -11,10 +11,144 @@ enum Command
     BC_STATS = 3
 };
 
+enum Platform
+{
+    CUSTOM = 0,
+    CHROMIUM = 1,
+    TELLSEQ= 2,
+    STLFR = 3
+};
+
+inline unsigned parseNumber(string & buffer, seqan::Iterator<const seqan::CharString, seqan::Rooted()>::Type & it)
+{
+    SEQAN_ASSERT(empty(buffer));
+    while (isNumeric(*it))
+    {
+        buffer += *it;
+        ++it;
+        if (!seqan::atEnd(it))
+        {
+            std::stringstream what;
+            what << "Structure-string ending with numeric value. Each position must be followed by an identifier (one of 'x', 'b', and 's'). See README for more details.";
+            SEQAN_THROW(ParseError(what.str()));
+        }
+    }
+    unsigned num = std::stoi(buffer);
+    seqan::clear(buffer);
+    return num;
+}
+
+// return -1 if the string ended, 0 if we are still on the same read and 1 if the read changed.
+inline int parseIdentifier(string & buffer,
+                           seqan::Iterator<const seqan::CharString, seqan::Rooted()>::Type & it,
+                           const bool & isFirstRead)
+{
+    SEQAN_ASSERT(empty(buffer));
+    if (*it == 'b' || *it == 'x' || *it == 's')
+        buffer += *it;
+    else
+    {
+        std::stringstream what;
+        what << "All positions in the structure-string must be followed by an an identifier (one of 'x', 'b', and 's'). See README for details.";
+        SEQAN_THROW(ParseError(what.str()));
+    }
+    ++it;
+    if (atEnd(it))
+        return -1;
+    else if (isNumeric(*it))
+    {
+        return 0;
+    }
+    else if ((*it == 'R' && isFirstRead) || (*it == 'F' && !isFirstRead))
+    {
+        return 1;
+    }
+    else if ((*it == 'F' && isFirstRead) || (*it == 'R' && !isFirstRead))
+    {
+        std::stringstream what;
+        what << "Structure-string contains forward read identifier \'" << *it << "\' two times, but must only contain each read identifier exactly once. See README for details.";
+        SEQAN_THROW(ParseError(what.str()));
+    }
+    else
+    {
+        std::stringstream what;
+        what << "Each position identifier in the structure-string must be followed by a numeric position, a read identifiert or the end of the string. See readme for details.";
+        SEQAN_THROW(ParseError(what.str()));
+    }
+}
+
+struct ReadStructure
+{
+    bool read;      // 0 = barcode in first read in pair, 1 = barcode in second read in pair
+
+    int barcodeLength;
+    seqan::String<seqan::Pair<unsigned> > barcodePositions;     // ( [startpos,endpos[, ...)
+    seqan::Pair<seqan::Pair<unsigned> > forwardSequencePositions; //<[seqStartPosR1, seqEndPosR2[, [seqStartPosR2, seqEndPosR2[>
+    seqan::Pair<seqan::Pair<unsigned> > reverseSequencePositions;
+
+    void ReadStructure(seqan::CharString s)
+    {
+        //F10b6x10b6x10b100sR...
+        SEQAN_ASSERT(!empty(s));
+        toLower(s);
+        std::stringstream what;
+        seqan::Iterator<const seqan::CharString, seqan::Rooted()>::Type it = seqan::begin(s);
+
+        // See if we currently are in the forward or reverse read.
+        bool noBarcodeYet = true;
+        bool currentlyOnSecondRead;
+        if (*it == 'f')
+            currentlyOnSecondRead = 0;
+        else if (*it == 'r')
+            currentlyOnSecondRead = 1;
+        else
+        {
+            what << "Invalid read specifier " << *it << " at start of structure-string. Must be one of 'F', 'R'. See README for more details.";
+            SEQAN_THROW(ParseError(what.str()));
+        }
+        // Now parse the rest of the structure String.
+        unsigned currentPos = 0;
+        string buffer;
+        reserve(buffer, 3);
+        while (!seqan::atEnd(it))
+        {
+            unsigned start = currentPos;
+            unsigned end = currentPos + parseNum(it);
+            int retCode = parseIdentifier(buffer, it, read);
+            if (buffer[0] == 'b')
+            {
+                if (noBarcodeYet)
+                    read = currentlyOnSecondRead;
+
+                seqan::appendValue(barcodePositions, seqan::Pair<unsigned>(start, end));
+            }
+            else if (buffer[0] == 's')
+                if (currentlyOnSecondRead)
+                    seqan::appendValue(reverseSequencePositions, seqan::Pair<unsigned>(start, end));
+                else
+                    seqan::appendValue(forwardSequencePositions, seqan::Pair<unsigned>(start, end));
+
+            if (retCode == -1)
+                ;// done
+
+            else if (retCode == 0)
+                clear(buffer);
+                continue; // continue on the same read
+            else
+            {
+                currentlyOnSecondRead = !currentlyOnSecondRead; //continue on the other read.
+                // Probably continue coding here
+            }
+        }
+    }
+};
+
 struct Options
 {
     Command cmd;
 
+    Platform          platform;
+    seqan::CharString structure;
     seqan::CharString whitelistFile;
     seqan::CharString fastqFile1;
     seqan::CharString fastqFile2;
